c53739250392b0dd54c6024f27596313d2df93ad
NAIK RAHIS <143823935+Nake101@users.noreply.github.com>
2023-09-26 14:45:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,94 @@
+// Chapter 20
+
+// var allLower = userInput.toLowerCase();
+
+// x=x.tolowerCase();
+
+// var allLower = userInput.toupperCase();
+
+// var originalString = "Yamaha company is one of the best companies in cars work";
+// var lowerCaseString = originalString.toLowerCase();
+
+// var userInput = prompt("Enter Your Company name");
+// var myArray = ["yamaha", "Honda", "suzuki", "superstar"]; 
+
+// if (i = 0 ,i<myArray.length, i++) {
+//   var lowerCaseString = myArray[i].toLowerCase();
+//   console.log(lowerCaseString);
+// }else {
+//   console.log("Invalid index");
+// }
+
+// var myString = "YoungStars"; 
+// var UpperCaseString = myString.toUpperCase();
+// alert(UpperCaseString);
+
+
+// var cityName = "kaRacHi";
+// var capitalisedCityName = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
+
+// console.log(capitalisedCityName);
+
+
+
+
+// chap 22-25
+
+
+// var sameWords = "captain";
+// var slicedWords = sameWords.slice(1, 3); 
+// console.log(slicedWords);
+
+
+
+// var sameWords = "captain";
+// var characterCount = sameWords.length;
+
+// console.log(characterCount);
+
+
+
+
+
+// var myString = "Hello, World!";
+// var charCount = myString.length;
+// var slicedString = myString.slice(1, -3);
+// console.log("Original String:", myString);
+// console.log("Number of Characters:", charCount);
+// console.log("Sliced String:", slicedString);
+
+
+// var text = "To be or not to be.";
+// var indx = text.indexOf("be");
+// console.log(indx);
+
+// var text = "To be or not to be.";
+// var indx = text.lastIndexOf("be");
+// console.log(indx);
+
+
+// var text = "To be or not to go. That is the question. To go or not to go.";
+// var lastIndexOfGo = text.lastIndexOf("go");
+
+// if (lastIndexOfGo !== -1) {
+//   var indx = lastIndexOfGo;
+//   console.log("Index of the first character of the last 'go':", indx);
+// } else {
+//   console.log("'go' not found in the string.");
+// }
+
+
+
+// var roundedNumber = Math.round(yourNumber);
+
+// var origNum = 5.6; 
+// var roundNum = Math.ceil(origNum);
+
+
+// var origNum = .5; 
+// var roundNum = Math.floor(origNum);
+
+
+var randomNum = Math.floor(Math.random() * 50) + 1;
+
+console.log(randomNum);
